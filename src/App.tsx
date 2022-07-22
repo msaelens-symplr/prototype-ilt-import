3b2e7b3d905d9af889890fd05d4cc6bd9e53868a
Mike Saelens <msaelens@symplr.com>
2022-07-22 18:01:25 -0400

    Add fake data.

--- src/App.tsx
+++ src/App.tsx
@@ -41,9 +41,23 @@ function App() {
                         <SymplDataGrid title='Instructor Lead Classes' selectionMode='checkbox' infinite maxHeight='300px'>
                             <SymplDgHead sticky={true} slot='header'>
                                 <SymplDgRow>
-                                    <SymplDgHeaderCell>Class Name</SymplDgHeaderCell>
-                                    <SymplDgHeaderCell>Session</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>Class Title</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>Course Title</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>Session Title</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>Start Date &amp; Time</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>End Date &amp; Time</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>Location</SymplDgHeaderCell>
                                     <SymplDgHeaderCell>Room</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>Class Notes</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>Max Enroll</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>Min Enroll</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>Instructor ID</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>Coordinator</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>Originator</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>Type</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>Status</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>Credit Types</SymplDgHeaderCell>
+                                    <SymplDgHeaderCell>Credit Value</SymplDgHeaderCell>
                                 </SymplDgRow>
                             </SymplDgHead>
                             <SymplDgBody slot='body'>
@@ -51,21 +65,77 @@ function App() {
                                     <SymplDgCell>Intro to SQL</SymplDgCell>
                                     <SymplDgCell>#2</SymplDgCell>
                                     <SymplDgCell>301</SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
                                 </SymplDgRow>
                                 <SymplDgRow>
                                     <SymplDgCell>Intermediate SQL</SymplDgCell>
                                     <SymplDgCell>#2</SymplDgCell>
                                     <SymplDgCell>10B</SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
                                 </SymplDgRow>
                                 <SymplDgRow>
                                     <SymplDgCell>Advanced SQL</SymplDgCell>
                                     <SymplDgCell>#2</SymplDgCell>
                                     <SymplDgCell>22-b</SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
                                 </SymplDgRow>
                                 <SymplDgRow>
                                     <SymplDgCell>Intro to graphQL</SymplDgCell>
                                     <SymplDgCell>#2</SymplDgCell>
                                     <SymplDgCell>18</SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
+                                    <SymplDgCell></SymplDgCell>
                                 </SymplDgRow>
                             </SymplDgBody>
                             <SymplDgFoot slot='footer'>
